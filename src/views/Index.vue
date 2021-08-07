1eec0b02c50d154e49ad2ce53f78a2f5eaa500da
Miracle@pro <luomengfanchen@outlook.com>
2021-08-07 21:21:40 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="index-container">
+        <div class="index-top"></div>
+
+        <div class="index-btn-group">
+            <div class="index-btn"></div>
+            <div class="index-btn"></div>
+            <div class="index-btn"></div>
+            <div class="index-btn"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+}
+</script>
+
+<style scoped>
+.index-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-left: 71px;
+}
+.index-top {
+    background-color: #eee;
+    border-radius: 5px;
+    height: 506px;
+    margin-top: 50px;
+    width: 900px;
+}
+
+
+.index-btn-group {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 900px;
+}
+.index-btn {
+    background-color: #eee;
+    border-radius: 5px;
+    height: 150px;
+    margin-top: 20px;
+    width: 440px;
+}
+</style>
