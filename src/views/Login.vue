e2d5a56564c5732c515f95f3ee7e21156e28f33d
Miracle@pro <luomengfanchen@outlook.com>
2021-08-30 20:13:27 +0800

    feat: 添加了登录注册页面

---
+++ src/views/Login.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="login-container">
+        <div class="login-card">
+            <h2 class="login-title">欢迎登陆</h2>
+
+            <form id="form">
+                <div class="login-input">
+                    <label for="account" class="login-label">邮箱</label>
+                    <input type="text" class="login-input-text"/>
+                </div>
+
+                <div class="login-input">
+                    <label for="password" class="login-label">密码</label>
+                    <input type="password" class="login-input-text"/>
+                </div>
+
+                <input type="button" value="登陆" class="login-button"/>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+
+<style scoped>
+/* 登陆容器 */
+.login-container {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin-left: 74px;
+}
+
+/* 登陆卡片 */
+.login-card {
+    align-items: center;
+    background-color: #fff;
+    border: #dcdfe6 solid 1px;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 100px;
+    margin-top: 200px;
+}
+
+/* 登陆标题 */
+.login-title {
+    color: #f56c6c;
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+
+/* 登陆输入容器 */
+.login-input {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    margin-top: 20px;
+}
+.login-label {
+    color: #f56c6c;
+    font-size: 20px;
+}
+.login-input-text {
+    border: #dcdfe6 solid 2px;
+    border-radius: 5px;
+    color: #303133;
+    font-size: 20px;
+    outline: none;
+    padding: 10px;
+}
+.login-input-text:focus {
+    border: #f56c6c solid 2px;
+    transition: all 0.2s ease 0s;
+}
+
+/* 登陆按钮 */
+.login-button {
+    background-color: #f56c6c;
+    border: #dcdfe6 solid 1px;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 20px;
+    margin-top: 50px;
+    padding: 10px;
+    width: 100%;
+}
+.login-button:hover {
+    background-color: #ff9696;
+    transition: all 0.2s ease 0s;
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="register-container">
+        <div class="register-card">
+            <h2 class="register-title">欢迎注册</h2>
+
+            <form id="form">
+                <div class="register-input">
+                    <label for="account" class="register-label">昵称</label>
+                    <input type="text" class="register-input-text"/>
+                </div>
+
+                <div class="register-input">
+                    <label for="account" class="register-label">邮箱</label>
+                    <input type="text" class="register-input-text"/>
+                </div>
+
+                <div class="register-input">
+                    <label for="password" class="register-label">密码</label>
+                    <input type="password" class="register-input-text"/>
+                </div>
+
+                <div class="register-input">
+                    <label for="password" class="register-label">确认密码</label>
+                    <input type="password" class="register-input-text"/>
+                </div>
+
+                <div class="register-input">
+                    <label for="account" class="register-label">出生日期</label>
+                    <input type="text" class="register-input-text"/>
+                </div>
+
+                <div class="register-input">
+                    <label for="account" class="register-label">自我介绍</label>
+                    <textarea class="register-input-text register-input-textarea"/>
+                </div>
+
+                <input type="button" value="注册" class="register-button"/>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Register'
+}
+</script>
+
+<style scoped>
+/* 注册容器 */
+.register-container {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin-left: 74px;
+}
+
+/* 注册卡片 */
+.register-card {
+    align-items: center;
+    background-color: #fff;
+    /* border: #dcdfe6 solid 1px; */
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 50px;
+    padding: 100px;
+    width: 900px;
+}
+
+/* 注册标题 */
+.register-title {
+    color: #f56c6c;
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+
+/* 注册输入容器 */
+.register-input {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    margin-top: 20px;
+}
+.register-label {
+    color: #f56c6c;
+    font-size: 20px;
+}
+.register-input-text {
+    border: #dcdfe6 solid 2px;
+    border-radius: 5px;
+    color: #303133;
+    font-size: 20px;
+    outline: none;
+    padding: 10px;
+    width: 500px;
+}
+.register-input-text:focus {
+    border: #f56c6c solid 2px;
+    transition: all 0.2s ease 0s;
+}
+.register-input-textarea {
+    height: 200px;
+    resize: none;
+}
+
+/* 注册按钮 */
+.register-button {
+    background-color: #f56c6c;
+    border: #dcdfe6 solid 1px;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 20px;
+    margin-top: 50px;
+    padding: 10px;
+    width: 100%;
+}
+.register-button:hover {
+    background-color: #ff9696;
+    transition: all 0.2s ease 0s;
+}
+</style>
