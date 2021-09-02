07c19115706d23c31c95e1d01ad40ef83fecc2c0
Miracle@pro <luomengfanchen@outlook.com>
2021-09-02 20:21:04 +0800

    feat: 添加了用户信息页面，及投稿选择页面

---
+++ src/views/user/Index.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="user-container">
+        <div class="user-card">
+            <div class="user-title-container">
+                <h2 class="user-title">用户信息</h2>
+            </div>
+
+            <div class="user-descript-container">
+                <div>
+                    <p class="user-descript-content">
+                        昵称:
+                        <input type="text" readonly class="user-descript-input" />
+                    </p>
+                    <p class="user-descript-content">
+                        邮箱:
+                        <input type="text" readonly class="user-descript-input" />
+                    </p>
+                    <p class="user-descript-content">
+                        生日:
+                        <input type="text" readonly class="user-descript-input" />
+                    </p>
+                </div>
+                <img src="" class="user-avator" />
+            </div>
+
+            <textarea readonly class="user-intro"></textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserIndex'
+}
+</script>
+
+<style scoped>
+.user-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 75px;
+}
+
+.user-card {
+    border: #DCDFE6 solid 1px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    margin-top: 100px;
+    width: 1200px;
+}
+
+.user-title-container {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin: 50px 0px;
+}
+.user-title {
+    color: #f56c6c;
+    font-size: 50px;
+}
+
+.user-descript-container {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 50px 100px 10px 100px;
+    width: 1000px;
+}
+
+.user-descript-content {
+    color: #303133;
+    font-size: 30px;
+    padding: 20px;
+}
+
+.user-descript-input {
+    border: #DCDFE6 solid 2px;
+    border-radius: 5px;
+    color: #303133;
+    font-size: 20px;
+    outline: none;
+    padding: 5px;
+}
+.user-descript-input:focus {
+    border: #f56c6c solid 2px;
+}
+
+.user-avator {
+    border-radius: 50%;
+    height: 210px;
+    width: 140px;
+}
+
+.user-intro {
+    border: #DCDFE6 solid 2px;
+    border-radius: 5px;
+    font-size: 30px;
+    height: 300px;
+    margin: 10px 100px 50px 100px;
+    outline: none;
+    padding: 10px;
+    resize: none;
+    width: 1000px;
+}
+.user-intro:focus {
+    border: #f56c6c solid 2px;
+}
+</style>
---
+++ src/views/user/Upload.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="upload-container">
+        <div class="upload-card">
+            <div class="upload-button">
+                <h3 class="upload-title">视频投稿</h3>
+                <router-link
+                    to="/user/upload/video"
+                    class="upload-link"
+                    style="background-color: #409EFF"
+                    >投稿</router-link
+                >
+            </div>
+            <div class="upload-button">
+                <h3 class="upload-title">音乐投稿</h3>
+                <router-link
+                    to="/user/upload/music"
+                    class="upload-link"
+                    style="background-color: #67C23A"
+                    >投稿</router-link
+                >
+            </div>
+            <div class="upload-button">
+                <h3 class="upload-title">文章投稿</h3>
+                <router-link
+                    to="/user/upload/article"
+                    class="upload-link"
+                    style="background-color: #E6A23C"
+                    >投稿</router-link
+                >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Upload'
+}
+</script>
+
+<style scoped>
+.upload-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 75px;
+}
+
+.upload-card {
+    align-items: center;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: row;
+    height: 500px;
+    justify-content: space-between;
+    margin-top: 100px;
+    padding: 100px;
+    width: 1000px;
+}
+
+.upload-button {
+    align-items: center;
+    border: #dcdfe6 solid 2px;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    height: 500px;
+    justify-content: space-around;
+    width: 300px;
+}
+.upload-button:hover {
+    box-shadow: 0px 3px 5px 0px #909399;
+}
+
+.upload-title {
+    color: #f56c6c;
+    font-size: 30px;
+}
+
+.upload-link {
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 20px;
+    padding: 10px 40px;
+    text-decoration: none;
+}
+.upload-link:hover {
+    box-shadow: 0px 3px 3px 0px #909399;
+}
+</style>
