fab897903377b060bdac3a59de555f80f1fa810a
Miracle@pro <luomengfanchen@outlook.com>
2021-09-23 16:37:38 +0800

    feat: 添加了搜索视频及音乐功能

---
+++ src/views/Search.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="search-container">
+        <div class="title-container">
+            <h2 class="search-title">
+                搜索视频结果
+            </h2>
+        </div>
+        <div class="card-container">
+            <VideoCard :videoInfo="videoList"></VideoCard>
+        </div>
+
+        <div class="title-container">
+            <h2 class="search-title">
+                搜索音乐结果
+            </h2>
+        </div>
+        <div class="card-container">
+            <MusicCard :musicInfo="musicList"></MusicCard>
+        </div>
+
+        <div class="title-container">
+            <h2 class="search-title">
+                搜索专栏结果
+            </h2>
+        </div>
+        <ArticleCard></ArticleCard>
+    </div>
+</template>
+
+<script>
+import VideoCard from '../components/video-card.vue'
+import MusicCard from '../components/music-card.vue'
+import ArticleCard from '../components/article-card.vue'
+import { normalAxios } from '../plugins/axios.js'
+
+export default {
+    name: 'Search',
+    data() {
+        return {
+            videoList: [],
+            musicList: []
+        }
+    },
+    components: {
+        VideoCard,
+        MusicCard,
+        ArticleCard
+    },
+    mounted() {
+        // 搜索视频信息
+        normalAxios
+            .get('/search?type=video&q=' + this.$route.params.value)
+            .then((response) => {
+                this.videoList = response.data.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        // 搜索音乐信息
+        normalAxios
+            .get('/search?type=music&q=' + this.$route.params.value)
+            .then((response) => {
+                this.musicList = response.data.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+}
+</script>
+
+<style scoped>
+/* 搜索容器 */
+.search-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 74px;
+}
+
+.title-container {
+    border-bottom: #409eff solid 3px;
+    margin-top: 100px;
+    width: 1200px;
+}
+.search-title {
+    color: #303133;
+    font-size: 30px;
+    letter-spacing: 10px;
+    text-align: left;
+}
+
+.card-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 1200px;
+}
+</style>
