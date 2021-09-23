7f36d8ec02fa293981c299a68a4c125fb6f38b1a
Miracle@pro <luomengfanchen@outlook.com>
2021-09-23 18:16:24 +0800

    feat: 存储用户头像，及方便的管理baseURL

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,35 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        loginStatus: window.sessionStorage.getItem('token')
+        loginStatus: window.sessionStorage.getItem('token'),
+        userAvator: window.sessionStorage.getItem('avator'),
+        baseURL: 'http://127.0.0.1:8081'
     },
     getters: {
         getLoginStatus: (state) => {
             return state.loginStatus
+        },
+        getAvator: (state) => {
+            return state.userAvator
+        },
+        getbaseURL: (state) => {
+            return state.baseURL
         }
     },
     mutations: {
         setLoginStatus: (state, status) => {
             state.loginStatus = status
+        },
+        setAvator: (state, url) => {
+            state.userAvator = url
         }
     },
     actions: {
         asyncLoginStatus: (context, status) => {
             context.commit('setLoginStatus', status)
+        },
+        asyncAvator: (context, url) => {
+            context.commit('setAvator', url)
         }
     }
 })
